29da6d10441e7d341bd4419b226a900e72697049
Barry Ivan Silber <age.ef.ess@gmail.com>
2019-06-29 00:37:55 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -15,7 +15,7 @@ def hopper
           :languages => ["C"]
         }
      }
-
+return programmer_hash[:grace_hopper]
 
 end
 
@@ -37,6 +37,9 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     
+    return programmer_hash[:alan_kay][:known_for]
+
 end
 
 def dennis_ritchies_language
@@ -55,6 +58,9 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     
+return programmer_hash[:dennis_ritchie][:languages][0]
+
 end
 
 def adding_matz
